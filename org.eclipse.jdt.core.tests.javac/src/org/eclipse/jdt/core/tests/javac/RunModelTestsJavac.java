c11a89059ad126265572cd7cb5a350cfb036bd26
David Thompson <davthomp@redhat.com>
2024-08-12 16:29:03 -0400

    Enable model tests for javac
    This runs the test suite for the IJavaElement model using the javac
    compiler.
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

---
+++ org.eclipse.jdt.core.tests.javac/src/org/eclipse/jdt/core/tests/javac/RunModelTestsJavac.java
@@ -0,0 +1,11 @@
+package org.eclipse.jdt.core.tests.javac;
+
+import org.eclipse.jdt.core.tests.model.AllJavaModelTests;
+
+public class RunModelTestsJavac extends AllJavaModelTests {
+
+	public RunModelTestsJavac(String name) {
+		super(name);
+	}
+
+}
